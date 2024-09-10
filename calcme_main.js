625b1e3c4d14510c64133da0e8edb4c67ab73d30
Eduard Voloshyn <edward.voloshin@gmail.com>
2024-09-10 22:32:25 +0300

    Update to 0.4 version
    Update to 0.4 version
    
    - add `maxPosibbleTotal`
    - add logic to generate negative numbers also
    - change logic accordingly
    - change UI
    - change preset `set_0`

--- calcme_main.js
+++ calcme_main.js
@@ -13,21 +13,32 @@ let minValue = 2;
 let maxValue = 6;
 let delaySeconds = 1;
 let total = 0;
+let maxPosibbleTotal=0; 
 
 let settings = {
     currentPlayer: 0,
 
     predifinedSettings: [
+        // set_debug = {
+        //     description: "Debug preset",
+        //     iterations: 5,
+        //     maxPosibbleTotal: 20,
+        //     minValue: 1,
+        //     maxValue: 9,
+        //     delaySeconds: 1
+        // },
         set_0 = {
             description: "Easy 1",
             iterations: 10,
+            maxPosibbleTotal: 20,
             minValue: 1,
-            maxValue: 9,
-            delaySeconds: 3
+            maxValue: 19,
+            delaySeconds: 2
         },
         set_1 = {
             description: "Middle 1",
             iterations: 20,
+            maxPosibbleTotal: 50,
             minValue: 1,
             maxValue: 20,
             delaySeconds: 2
@@ -35,6 +46,7 @@ let settings = {
         set_1 = {
             description: "Hard 1",
             iterations: 30,
+            maxPosibbleTotal: 100,
             minValue: 10,
             maxValue: 50,
             delaySeconds: 2
@@ -88,31 +100,36 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-
+// Logic
 function getParameters() {
     iterations = Number(document.getElementById("iterations").value);
     minValue = Number(document.getElementById("minValue").value);
     maxValue = Number(document.getElementById("maxValue").value);
     delaySeconds = Number(document.getElementById("delaySecondsValue").value);
+    maxPosibbleTotal = Number(document.getElementById("maxPossibleTotalValue").value);
 }
+
 function setUpParameters() {
     setFieldValue("iterations", iterations);
     setFieldValue("minValue", minValue);
     setFieldValue("maxValue", maxValue);
     setFieldValue("delaySecondsValue", delaySeconds);
+    setFieldValue("maxPossibleTotalValue", maxPosibbleTotal);
 }
 
 function initGame() {
     iterations = settings.predifinedSettings[0].iterations;
     minValue = settings.predifinedSettings[0].minValue;
     maxValue = settings.predifinedSettings[0].maxValue;
-    delaySeconds = settings.predifinedSettings[0].delaySeconds; 
+    delaySeconds = settings.predifinedSettings[0].delaySeconds;
+    maxPosibbleTotal = settings.predifinedSettings[0].maxPosibbleTotal;
 
     setUpParameters();
    
+    showUiLabel("number" , "");
     disableEnableButton("myResultValue",DISABLE);
-    disableEnableButton("inputResultLabel",DISABLE);
-    disableEnableButton("checkResultButton",DISABLE);
+    disableEnableButton("inputResult_Label",DISABLE);
+    disableEnableButton("checkResult_Button",DISABLE);
 }
 
 // UI
@@ -142,33 +159,53 @@ function sleep(ms) {
 async function startGame(params) {
     let index=0;
     let new_number=0;
+    let isPositive=true;
     total = 0;
 
     getParameters();
+    
 
     disableEnableButton("myResultValue",DISABLE);
-    disableEnableButton("inputResultLabel",DISABLE);
-    disableEnableButton("checkResultButton",DISABLE);
+    disableEnableButton("inputResult_Label",DISABLE);
+    disableEnableButton("checkResult_Button",DISABLE);
 
     for (index; index < iterations; index++){
 
         console.log('iteration:' + (index+1))
-        await sleep(delaySeconds*MILLISECONDS) // Pause for 2 seconds
+        await sleep(delaySeconds*MILLISECONDS) // Pause for seconds
         
         
         // setTimeout(function(new_number) {
-        new_number = minValue+getRandomInt(maxValue-minValue);
+        console.log('isPositive: '+ isPositive);
+        do {
+            new_number = minValue+getRandomInt(maxValue-minValue);
+            
+            if (isPositive == false) {
+                new_number=new_number* (-1);
+                
+            }
+            console.log('new number TRY: '+ new_number);
+            
+        } while (total+new_number>Math.abs(maxPosibbleTotal) || (total+new_number<1));
+       
         console.log('next number: '+ new_number);
 
         total += new_number;
+        isPositive=!isPositive;
+        
         console.log('total: '+ total);
+        console.log('------------------------');
+
         showUiLabel("number" , new_number);
       
     }
 
+    showUiLabel("number" , "");
     disableEnableButton("myResultValue",ENABLE);
-    disableEnableButton("inputResultLabel",ENABLE);
-    disableEnableButton("checkResultButton",ENABLE);
+    disableEnableButton("inputResult_Label",ENABLE);
+    disableEnableButton("checkResult_Button",ENABLE);
+    
+    
 }
 
 function checkResult() {
